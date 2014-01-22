bffa797d97bd7ce2e05bcfef6ae635e683f9db8d
L-G-K <13010401@i202m04.istic.univ-rennes1.fr>
2014-01-22 16:45:51 +0100

    Test

---
+++ tpmaven/src/main/java/fr/istic/master1/sir/tpmaven/App.java
@@ -0,0 +1,17 @@
+package fr.istic.master1.sir.tpmaven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	
+	public void inutile(){
+		System.out.println("Coucou cocu !");
+	}
+	
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+    }
+}
